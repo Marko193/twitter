f78ede61b6cddd5c4ee1299bcca7367ddd863690
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-03-12 00:36:35 +0200

    Posts are shown & begin to implement the Like btn

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,18 @@
 $(document).ready(() => {
-    $.get("/api/posts", { followingOnly: true }, results => {
+    $.get("/api/posts", results => {
         outputPosts(results, $(".postsContainer"));
-    })
-})
+    });
+});
+
+function outputPosts(results, container) {
+    container.html("");
+
+    results.forEach(result => {
+        let html = createPostHtml(result);
+        container.append(html);
+    });
+
+    if (results.length == 0) {
+        container.append("<span class='noResults'>No tweets :-(</span>");
+    }
+}
