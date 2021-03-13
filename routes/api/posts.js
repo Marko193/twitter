c4dbe04dc4058bd3c1f9ae550b8d042b24061977
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-03-13 16:53:09 +0200

    Implemented retweeting of the posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,16 +9,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
     Post.find()
-    .populate("postedBy")
-    .sort({ "createdAt": -1 })
-    .then(results => res.status(200).send(results))
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
-})
+        .populate("postedBy")
+        .populate('retweetData')
+        .sort({ "createdAt": -1 })
+        .then(async results => {
+            results = await User.populate(results, { path: 'retweetData.postedBy' });
+            res.status(200).send(results);
+        })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        });
+});
 
-router.post("/", async (req, res, next) => {
+router.post("/", async(req, res, next) => {
 
     if (!req.body.content) {
         console.log("Content param not sent with request");
@@ -31,18 +35,18 @@ router.post("/", async (req, res, next) => {
     }
 
     Post.create(postData)
-    .then(async newPost => {
-        newPost = await User.populate(newPost, { path: "postedBy" })
+        .then(async newPost => {
+            newPost = await User.populate(newPost, { path: "postedBy" })
 
-        res.status(201).send(newPost);
-    })
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
+            res.status(201).send(newPost);
+        })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        })
 })
 
-router.put("/:id/like", async (req, res, next) => {
+router.put("/:id/like", async(req, res, next) => {
 
     var postId = req.params.id;
     var userId = req.session.user._id;
@@ -52,18 +56,66 @@ router.put("/:id/like", async (req, res, next) => {
     var option = isLiked ? "$pull" : "$addToSet";
 
     // Insert user like
-    req.session.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId } }, { new: true})
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
+    req.session.user = await User.findByIdAndUpdate(userId, {
+            [option]: { likes: postId }
+        }, { new: true })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        });
 
     // Insert post like
-    var post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId } }, { new: true})
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
+    let post = await Post.findByIdAndUpdate(postId, {
+            [option]: { likes: userId }
+        }, { new: true })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        });
+
+    res.status(200).send(post)
+});
+
+router.post("/:id/retweet", async(req, res, next) => {
+    var postId = req.params.id;
+    var userId = req.session.user._id;
+
+    // Try and delete retweet
+    var deletedPost = await Post.findOneAndDelete({ postedBy: userId, retweetData: postId })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        })
+
+    var option = deletedPost != null ? "$pull" : "$addToSet";
+
+    var repost = deletedPost;
+
+    if (repost == null) {
+        repost = await Post.create({ postedBy: userId, retweetData: postId })
+            .catch(error => {
+                console.log(error);
+                res.sendStatus(400);
+            })
+    }
+
+    // Insert user like
+    req.session.user = await User.findByIdAndUpdate(userId, {
+            [option]: { retweets: repost._id }
+        }, { new: true })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        })
+
+    // Insert post like
+    var post = await Post.findByIdAndUpdate(postId, {
+            [option]: { retweetUsers: userId }
+        }, { new: true })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(400);
+        })
 
 
     res.status(200).send(post)
--- schemas/PostSchema.js
+++ schemas/PostSchema.js
@@ -6,8 +6,10 @@ const PostSchema = new Schema({
     content: { type: String, trim: true },
     postedBy: { type: Schema.Types.ObjectId, ref: 'User' },
     pinned: Boolean,
-    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    retweetUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    retweetData: { type: Schema.Types.ObjectId, ref: 'Post' }
 }, { timestamps: true });
 
-var Post = mongoose.model('Post', PostSchema);
+let Post = mongoose.model('Post', PostSchema);
 module.exports = Post;
